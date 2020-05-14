f8e0f3ba178e9c5bb9948a041b15a462df75db0d
Pjus <wnstjd117@naver.com>
2020-05-14 16:58:02 +0900

    sds

--- hello/views.py
+++ hello/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+import pickle
 
 # Create your views here.
 
 def home(request):
     # return HttpResponse('Hello, Django!')
-    name01 = request.GET['name']
-    age02 = request.GET['age']
-    requestDict = request.GET
-    result = int(age02) + 5
+    x1 = request.GET['x1']
+    x2 = request.GET['x2']
+
+    emp_pkl4 = pickle.load(open('./pre4.pkl', 'rb'))
+    result = emp_pkl4.getPredict(int(x1), int(x2))
     requestDict = {'result_response':result}
     return JsonResponse(requestDict)
 
