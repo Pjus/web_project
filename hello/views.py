7abe93fa554613a784340af4297ac2bc0d572ef1
Pjus <wnstjd117@naver.com>
2020-05-14 16:23:35 +0900

    12

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello, Django!')
+    # return HttpResponse('Hello, Django!')
+    name01 = request.GET['name']
+    age02 = request.GET['age']
+    requestDict = request.GET
+    result = int(age02) + 5
+    requestDict = {'result_response':result}
+    return JsonResponse(requestDict)
 
